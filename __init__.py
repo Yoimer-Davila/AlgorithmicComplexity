388da171e026b1c0ad91288b28683c86ed41070a
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2023-05-25 14:29:35 -0500

    Update init file

--- __init__.py
+++ __init__.py
@@ -3,4 +3,12 @@ import sys
 from .graphs import *
 from .utilities import *
 
-sys.path.append('/AlgorithmicComplexity/')
+sys.path.append('./AlgorithmicComplexity')
+
+
+"""
+If you get error in Google Collab import run the bellow code at the beginning
+
+import sys
+sys.path.append('./AlgorithmicComplexity')
+"""
