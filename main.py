bb3d011a351fb928a595a7b1cdc763d398055e4e
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2023-05-25 20:57:32 -0500

    Sample with AdjacencyList manager

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
-from graphs import from_adjacency_file, show_graph, adjacency_list_graph, from_kruskal, kruskal, breadth_first_search_al
+from graphs import NamedAdjacencyList, show_graph, adjacency_list_graph, from_kruskal, kruskal, breadth_first_search_al
 
-ad_list, labels = from_adjacency_file("list.adjl", weighted=True, has_labels=True)
+labels = ['5to', 'Practicas', 'Diplomado', 'ChatGPT', 'Back', 'Front', 'Seguridad', 'QA', 'Gestion']
+ad_list = NamedAdjacencyList(labels, True)
+
+ad_list.connect('5to', 'ChatGPT', 2, True)
+ad_list.connect('5to', 'Diplomado', 10, True)
+ad_list.connect('5to', 'Practicas', 5, True)
+
+ad_list.connect('Practicas', 'Seguridad', 7, True)
+ad_list.connect('Practicas', 'Front', 2, True)
+ad_list.connect('Practicas', 'Gestion', 10, True)
+ad_list.connect('Practicas', 'Back', 3, True)
+ad_list.connect('Practicas', 'Diplomado', 5, True)
+
+ad_list.connect('Diplomado', 'ChatGPT', 10, True)
+
+ad_list.connect('Diplomado', 'Front', 4, True)
+ad_list.connect('Diplomado', 'Back', 4, True)
+ad_list.connect('Diplomado', 'Seguridad', 2, True)
+ad_list.connect('Diplomado', 'Gestion', 3, True)
+ad_list.connect('Diplomado', 'QA', 2, True)
+
+ad_list.connect('Back', 'Front', 1, True)
+ad_list.connect('Back', 'Seguridad', 3, True)
+
+ad_list.add_node('Back')
+
+
+print(ad_list)
 
 min_ad_list = kruskal(ad_list)
 path = breadth_first_search_al(from_kruskal(len(ad_list), min_ad_list), 0)
 print(path)
-graph = adjacency_list_graph(ad_list, weighted=True, path=path, labels=labels)
-show_graph(graph, size='10px,3px')
+graph = adjacency_list_graph(ad_list, weighted=True, path=path, labels=ad_list.labels(), layout='circo')
+show_graph(graph)
 
 
