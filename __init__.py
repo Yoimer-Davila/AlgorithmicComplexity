30a612337807e8ff05007cdd8658a9a656ccb929
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2023-05-25 20:51:24 -0500

    Fix import error

--- __init__.py
+++ __init__.py
@@ -1,14 +1,12 @@
 import sys
+sys.path.append('./AlgorithmicComplexity')
+
 
 from .graphs import *
 from .utilities import *
 
-sys.path.append('./AlgorithmicComplexity')
-
-
 """
 If you get error in Google Collab import run the bellow code at the beginning
-
 import sys
 sys.path.append('./AlgorithmicComplexity')
 """
